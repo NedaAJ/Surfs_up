176e87400c39ab46afd045ae76ca0c6f210a5866
NedaAJ <96074737+NedaAJ@users.noreply.github.com>
2022-02-24 20:38:12 -0500

    adding challenge files

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 #Set up Database
+# engine=create_engine("sqlite:///hawaii.sqlite", connect_args={"check_same_thread": False})
 engine=create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -21,6 +22,7 @@ app = Flask(__name__)
 #Setting up the welcome route
 @app.route("/")
 def welcome():
+    session = Session(engine)
     return(
     '''
     <xmp>
@@ -37,34 +39,40 @@ def welcome():
 @app.route('/api/v1.0/precipitation') 
 
 def precipitation():
+    session = Session(engine)
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
+    session.close
     return jsonify(precip)
 
 #Setting up the Stations route
 @app.route('/api/v1.0/stations')
 def stations():
+    session = Session(engine)
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
+    session.close
     return jsonify(stations=stations)
 
 #Setting up the Tobs route
 @app.route('/api/v1.0/tobs')
 def  temp_monthly():
+    session = Session(engine)
     prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
     results = session.query(Measurement.tobs).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >=prev_year).all()
     temps = list(np.ravel(results))
+    session.close
     return jsonify(temps=temps)
 
 #Setting up Statics Route
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
-
 def stats(start=None, end=None):
+    session = Session(engine)
     sel=[func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     
     if not end:
@@ -77,6 +85,7 @@ def stats(start=None, end=None):
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
+    session.close
     return jsonify(temps = temps)
 
 if __name__ == "__main__":
